34b31a2b4135c1099ebd8603811185013c324241
Ralph L <lettau.ralph@gmail.com>
2019-10-14 20:58:55 +0200

    Difficulty Enumeration

---
+++ src/main/java/de/qmsq/springrecipes/domain/Difficulty.java
@@ -0,0 +1,6 @@
+package de.qmsq.springrecipes.domain;
+
+public enum Difficulty {
+
+    EASY, MODERATE, HARD
+}
--- src/main/java/de/qmsq/springrecipes/domain/Recipe.java
+++ src/main/java/de/qmsq/springrecipes/domain/Recipe.java
@@ -17,11 +17,12 @@ public class Recipe {
     private String source;
     private String url;
     private String directions;
-    //todo add
-    // private Difficulty difficulty
 
     @Lob
     private Byte[] image;
+
+    @Enumerated(value = EnumType.STRING)
+    private Difficulty difficulty;
 
     @OneToOne(cascade = CascadeType.ALL)
     private Notes notes;
@@ -115,5 +116,13 @@ public class Recipe {
 
     public void setIngredients(Set<Ingredient> ingredients) {
         this.ingredients = ingredients;
+    }
+
+    public Difficulty getDifficulty() {
+        return difficulty;
+    }
+
+    public void setDifficulty(Difficulty difficulty) {
+        this.difficulty = difficulty;
     }
 }
