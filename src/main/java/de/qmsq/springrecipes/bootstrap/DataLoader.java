1b762de691c6b6ae03cc8d36e181abc09467586d
Ralph L <lettau.ralph@gmail.com>
2019-10-19 13:04:21 +0200

    Refactor to projekt lombok

--- src/main/java/de/qmsq/springrecipes/bootstrap/DataLoader.java
+++ src/main/java/de/qmsq/springrecipes/bootstrap/DataLoader.java
@@ -4,6 +4,7 @@ import de.qmsq.springrecipes.domain.*;
 import de.qmsq.springrecipes.repositories.CategoryRepository;
 import de.qmsq.springrecipes.repositories.RecipeRepository;
 import de.qmsq.springrecipes.repositories.UnitOfMeasureRepository;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.context.ApplicationListener;
 import org.springframework.context.event.ContextRefreshedEvent;
 import org.springframework.stereotype.Component;
@@ -13,6 +14,7 @@ import java.util.ArrayList;
 import java.util.List;
 import java.util.Optional;
 
+@Slf4j
 @Component
 public class DataLoader implements ApplicationListener<ContextRefreshedEvent> {
 
@@ -28,6 +30,7 @@ public class DataLoader implements ApplicationListener<ContextRefreshedEvent> {
 
     @Override
     public void onApplicationEvent(ContextRefreshedEvent contextRefreshedEvent) {
+        log.debug("Populating the data with bootstrap data");
         recipeRepository.saveAll(getRecipes());
     }
 
--- src/main/java/de/qmsq/springrecipes/controllers/IndexController.java
+++ src/main/java/de/qmsq/springrecipes/controllers/IndexController.java
@@ -1,10 +1,12 @@
 package de.qmsq.springrecipes.controllers;
 
 import de.qmsq.springrecipes.services.RecipeService;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
 
+@Slf4j
 @Controller
 public class IndexController {
 
@@ -16,6 +18,7 @@ public class IndexController {
 
     @RequestMapping({"", "/", "/index"})
     public String getIndexPage(Model theModel) {
+        log.debug("In the Index controller");
 
         theModel.addAttribute("recipes",  recipeService.getRecipes());
 
