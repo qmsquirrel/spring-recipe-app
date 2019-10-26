332bc683c3f7a9d7812766d9becbbe66e5b78d3f
Ralph L <lettau.ralph@gmail.com>
2019-10-26 16:26:03 +0200

    Delete recipe

--- src/main/java/de/qmsq/springrecipes/services/RecipeService.java
+++ src/main/java/de/qmsq/springrecipes/services/RecipeService.java
@@ -10,6 +10,7 @@ public interface RecipeService {
     Set<Recipe> getRecipes();
     Recipe findById(Long id);
     RecipeCommand findCommandById(Long id);
+    void deleteById(Long id);
 
     RecipeCommand saveRecipeCommand(RecipeCommand command);
 }
--- src/main/java/de/qmsq/springrecipes/services/RecipeServiceImpl.java
+++ src/main/java/de/qmsq/springrecipes/services/RecipeServiceImpl.java
@@ -64,4 +64,9 @@ public class RecipeServiceImpl implements RecipeService {
         log.debug("saved RecipeId: " + savedRecipe.getId());
         return recipeToRecipeCommand.convert(savedRecipe);
     }
+
+    @Override
+    public void deleteById(Long id) {
+        recipeRepository.deleteById(id);
+    }
 }
